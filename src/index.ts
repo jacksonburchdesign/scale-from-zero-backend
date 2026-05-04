abb22fb56816489d8ff8cef1f3e3690047f90dae
jacksonburchdesign <jacksonburch95@gmail.com>
2026-05-03 20:54:32 -0500

    firebase details

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as admin from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
 import { formatChangelogWithAI } from "./functions/aiChangelog.js";
 import { githubWebhook } from "./functions/githubWebhook.js";
 import { vercelWebhook } from "./functions/vercelWebhook.js";
 import { generateInvite } from "./functions/generateInvite.js";
 import { acceptInvite } from "./functions/acceptInvite.js";
 
-admin.initializeApp();
+initializeApp();
 
 export { formatChangelogWithAI, githubWebhook, vercelWebhook, generateInvite, acceptInvite };
